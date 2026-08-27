db5f7cf5f6a0e36c796cc07141d196f435fe8adf
nguyentantanpolo-blip <nguyentantanpolo-blip@users.noreply.github.com>
2026-08-27 20:14:07 +0700

    fix: cập nhật đường dẫn Facebook chính thức của Tanpolo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -115,7 +115,7 @@ export default function Footer() {
 
               {/* Facebook */}
               <a
-                href="https://facebook.com"
+                href="https://www.facebook.com/tanpolovietnam"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="social-channel-item"
